c573c9efd8006c6783b7f67e6232fc5b73aa5228
HicH987 <diabhicham987@gmail.com>
2023-04-02 10:15:45 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+from flask_cors import CORS
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/add_hello', methods=['POST'])
+def add_hello():
+    data = request.get_json()
+    input_string = data['input_string']
+    output_string = input_string + ' hello'
+    print(output_string)
+    response = {'output_string': output_string}
+    return jsonify(response)
+
+
+@app.route('/api/upload-image', methods=['POST'])
+def upload_image():
+    image_file = request.files['image']
+    image = cv2.imdecode(np.frombuffer(image_file.read(), np.uint8), cv2.IMREAD_UNCHANGED)
+    cv2.imwrite('./uploaded_image.jpg', image)
+    return {'message': 'Image uploaded successfully'}
+
+if __name__ == '__main__':
+    app.run(debug=True)
