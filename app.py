36de9b8c11a45dde6b4646e70051476cdf2e1341
HicH987 <diabhicham987@gmail.com>
2023-04-04 15:58:16 +0100

    delete some useless function

--- app.py
+++ app.py
@@ -7,40 +7,33 @@ from utils.face_id_functions import add_face, run_identification
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/test/')
-def test() :
-    return '!!!! test flask app !!!!'
+
+@app.route("/test/")
+def test():
+    return "!!!! test flask app !!!!"
 
 
-@app.route('/api/upload-image', methods=['POST'])
-def upload_image():
-    image_file = request.files['image']
-    image = cv2.imdecode(np.frombuffer(image_file.read(), np.uint8), cv2.IMREAD_UNCHANGED)
-    cv2.imwrite('./uploaded_image.jpg', image)
-    return {'message': 'Image uploaded successfully'}
-
-
-@app.route('/api/face-identification', methods=['POST'])
+@app.route("/api/face-identification", methods=["POST"])
 def face_identification():
-    image_file = request.files['image']
-    image = cv2.imdecode(np.frombuffer(image_file.read(), np.uint8), cv2.IMREAD_UNCHANGED)
+    image_file = request.files["image"]
+    image = cv2.imdecode(
+        np.frombuffer(image_file.read(), np.uint8), cv2.IMREAD_UNCHANGED
+    )
     face_name = run_identification(image)
-    
-    return {'name': f'{face_name}'}
+
+    return {"name": f"{face_name}"}
 
 
-@app.route('/api/add-face-name', methods=['POST'])
+@app.route("/api/add-face-name", methods=["POST"])
 def add_face_name():
     data = request.get_json()
-    text_input = data['textInput']
-    
+    text_input = data["textInput"]
+
     add_face(text_input)
-    
-    response = {'message': f'The face was named: {text_input}'}
+
+    response = {"message": f"The face was named: {text_input}"}
     return jsonify(response)
 
 
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
