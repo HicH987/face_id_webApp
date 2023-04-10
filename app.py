47e1f29a2ea358291db91f15036e0c660abc5374
HicH987 <diabhicham987@gmail.com>
2023-04-10 05:14:29 +0100

    update deployment

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 import cv2
+import argparse
 import numpy as np
+
 from flask_cors import CORS
 from flask import Flask, request, jsonify
+
 from utils.face_id_functions import add_face, run_identification
+
 
 app = Flask(__name__)
 CORS(app)
 
 
-@app.route("/test/")
+@app.route("/test")
 def test():
     return "!!!! test flask app !!!!"
 
@@ -35,5 +39,16 @@ def add_face_name():
     return jsonify(response)
 
 
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--url', help='ACCESS LINK', default=None)
+
+args = parser.parse_args()
+
 if __name__ == "__main__":
+    if args.url:
+        print('\n')
+        for rule in list(app.url_map.iter_rules())[1:]:
+                print(str(args.url)+str(rule))
+        print('\n')
     app.run(debug=True)
